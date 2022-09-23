c13b7e8ee916f0ca1fbc3fc68f53940a6b888557
faridbayram <faridbayramgit@gmail.com>
2022-09-23 21:29:33 +0400

    _

--- CProjects/AnotherSimpleCProgram/main.c
+++ CProjects/AnotherSimpleCProgram/main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int custom_pow(int number, int power){
+
+    int result = 1;
+
+    int i;
+    for(i = 0; i < power; i ++)
+    {
+        result *= number;
+    }
+
+    return result;
+}
+
 int main() {
     int integer1;
     int integer2;
@@ -11,8 +24,11 @@ int main() {
     printf("Enter second integer\n");   // prompt
     scanf("%d", &integer2);   // read an integer
 
-    int sum;
-    sum = integer1 + integer2;
+    int sum = integer1 + integer2;
 
     printf("Sum is %d\n", sum);
+
+    printf("%d\n", custom_pow(5, 2));
+    printf("%d\n", custom_pow(4, 6));
+
 }
