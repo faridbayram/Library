e8ec0ebc12c5864a79e39fba030b268c413408a4
faridbayram <faridbayramgit@gmail.com>
2022-10-02 22:56:25 +0400

    _

--- CProjects/AnotherSimpleCProgram/main.c
+++ CProjects/AnotherSimpleCProgram/main.c
@@ -3,9 +3,21 @@
 
 int main(void)
 {
-    printf("%d\n", SHRT_MAX);
-    printf("%d\n", INT_MIN);
-    printf("%li\n", LONG_MIN);
-    printf("%lli\n", LONG_LONG_MAX);
+    //printf("%d\n", SHRT_MAX);
+    //printf("%d\n", INT_MIN);
+    //printf("%li\n", LONG_MIN);
+    //printf("%lli\n", LONG_LONG_MAX);
+
+    int a = 2 < 4 != 0;
+
+    printf("%d\n", a);
+
+    int i = 0;
+
+    if(-231){
+        puts("condition checked");
+    }
+
+
 }
 
