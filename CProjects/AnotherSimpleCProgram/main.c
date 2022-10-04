7c10af200e5581768b68c18f99ce090b5bf4ac14
faridbayram <faridbayramgit@gmail.com>
2022-10-05 02:06:43 +0400

    _

--- CProjects/AnotherSimpleCProgram/main.c
+++ CProjects/AnotherSimpleCProgram/main.c
@@ -3,21 +3,42 @@
 
 int main(void)
 {
-    //printf("%d\n", SHRT_MAX);
-    //printf("%d\n", INT_MIN);
-    //printf("%li\n", LONG_MIN);
-    //printf("%lli\n", LONG_LONG_MAX);
+    //int x, y, z;
+    //int entered_items_count = scanf("%d%d%d", &x, &y, &z);
 
-    int a = 2 < 4 != 0;
+    //int a;
+    //while((a = getchar()) != EOF){
+    //    printf("%d\n", a);
+    //}
 
-    printf("%d\n", a);
-
-    int i = 0;
-
-    if(-231){
-        puts("condition checked");
+    unsigned int sum = 0, count = 0;
+    int i;
+    for(i = 1; i <= 99; i += 2){
+        sum += i;
+        count += 1;
     }
 
+    printf("sum is : %d\n", sum);
+    printf("count is : %d\n", count);
 
+    double d = 333.546372;
+
+    printf("right justified -> %20f\n", d);
+    printf("right justified -> %20.4f\n", d);
+    printf("left justified -> %-20.4f\n", d);
+
+
+    for(i = 1; i <= 20; i ++){
+        if(i % 5 == 0){
+            printf("%d\n", i);
+        }
+        else{
+            printf("%d\t", i);
+        }
+    }
+
+    for (double y = 0.1; y != 1.0; y += 0.1) {
+        printf("%f\n", y);
+    }
 }
 
