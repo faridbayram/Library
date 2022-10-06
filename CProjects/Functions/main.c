c18cdec1b3fa6864491efe000de06f3f677b0082
faridbayram <faridbayramgit@gmail.com>
2022-10-06 22:51:06 +0400

    _

---
+++ CProjects/Functions/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int square(int x);
+void voidFunction();
+
+int main() {
+
+}
+
+int square(int x){
+    return x * x;
+}
+
+void voidFunction(){
+
+}
---
+++ CProjects/PrintDiamondProject/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+    int n = 50;
+
+    int i;
+    for(i = 1; i <= n; i ++)
+    {
+        int k;
+
+        for(k = i; k < n; k ++){
+            printf("%s", " ");
+        }
+
+        for(k = 1; k <= i; k ++){
+            printf("%s", "*");
+        }
+
+        for(k = 1; k < i; k ++){
+            printf("%s", "*");
+        }
+
+        for(k = i; k < n; k ++){
+            printf("%s", " ");
+        }
+
+        puts("");
+    }
+
+    for(i = n; i >= 1; i --)
+    {
+        int k;
+
+        for(k = i; k <= n; k ++){
+            printf("%s", " ");
+        }
+
+        for(k = i; k >= 2; k --){
+            printf("%s", "*");
+        }
+
+        for(k = i; k > 2; k --){
+            printf("%s", "*");
+        }
+
+        for(k = i; k < n; k ++){
+            printf("%s", " ");
+        }
+
+        puts("");
+    }
+
+    scanf("%d", &n);
+}
