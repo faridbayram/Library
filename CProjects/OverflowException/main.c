6df9be6314bf7bab6a88f12e6f5187846dec76ea
faridbayram <faridbayramgit@gmail.com>
2022-10-16 21:58:08 +0400

    _

--- CProjects/OverflowException/main.c
+++ CProjects/OverflowException/main.c
@@ -2,9 +2,16 @@
 #include <limits.h>
 #include <malloc.h>
 
+double pow(double number, int exp){
+    if(exp == 1)
+        return number;
+
+    return number * pow(number, exp - 1);
+}
+
 int main() {
-    int* ptr = (int*) malloc (sizeof (int));
-    *ptr = 3;
+    int result = pow(12, 7);
+    printf("%d\n", result);
 
-
+    return 0;
 }
