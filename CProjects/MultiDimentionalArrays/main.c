73bf0bf8cdff1b70fe4fd1a59183daadb0d50e6f
faridbayram <faridbayramgit@gmail.com>
2022-11-21 23:29:46 +0400

    _

--- CProjects/MultiDimentionalArrays/main.c
+++ CProjects/MultiDimentionalArrays/main.c
@@ -1,15 +1,42 @@
 #include <stdio.h>
 
-int main() {
-    char fcs[23];
-    scanf("%s", fcs);
+#define RESULT_SIZE 17
 
-    int i;
-    for(i = 0; i < 50; i ++)
-    {
-        printf("%c\n", fcs[i]);
+char* isPalindromeRecursion(char text[], int leftIndex, int rightIndex){
+    if(text[leftIndex] != text[rightIndex]){
+        return "is not polindrome";
     }
-    //printf(fcs, 5, 15);
 
-    return 0;
+    leftIndex ++;
+    rightIndex --;
+
+    if(leftIndex >= rightIndex)
+        return "is polindrome    ";
+
+    return isPalindromeRecursion(text, leftIndex + 1, rightIndex - 1);
+}
+
+char* isPalindrome(char text[], int leftIndex, int rightIndex){
+    if(leftIndex >= rightIndex)
+        return "invalid input    ";
+
+    return isPalindromeRecursion(text, leftIndex, rightIndex);
+}
+
+void printIsPolindrome(const char* string){
+    for(size_t i = 0; i < RESULT_SIZE; i ++){
+        printf("%c",*(string + i));
+    }
+    puts("");
+}
+
+int main() {
+    char* example1 = isPalindrome("radar", 0, 4);
+    printIsPolindrome(example1);
+
+    char* example2 = isPalindrome("able was i ere i saw elba", 0, 24);
+    printIsPolindrome(example2);
+
+    char* example3 = isPalindrome("a man a plan a canal panama", 0, 26);
+    printIsPolindrome(example3);
 }
