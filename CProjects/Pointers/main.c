9dd3dcf4b25bafa1cadd0e4387ef5ac8bcb875e4
faridbayram <faridbayramgit@gmail.com>
2022-11-27 20:55:07 +0400

    _

--- CProjects/Pointers/main.c
+++ CProjects/Pointers/main.c
@@ -1,15 +1,42 @@
 #include <stdio.h>
 
-void arrayFunc(int *arrPtr){
-    arrPtr[3] = 3;
+void myFunc(int *arrPtr){
+    for(int i = 0; i < 3; i ++)
+    {
+        arrPtr[i] = i + 1;
+    }
+
+    int newArray[] = {5,4,3};
+    arrPtr = newArray;
+}
+
+void printArray(int arr[]){
+    for(int i = 0; i < 3; i ++)
+    {
+        printf("%d\t", arr[i]);
+    }
+    puts("");
+
+    arr[2] = 2;
+    int someOtherArr[] = {1,1,1,1,11,1};
+    arr = someOtherArr;
+}
+
+void f(int *ptr){
+    *ptr = 15;
+    int a = 1;
 }
 
 int main() {
-    int arr[5];
+    int i = 3;
+    printf("%d\n", i);
+    const int *iPtr = &i;
+    int k = 3;
+    iPtr = &k;
 
-    arrayFunc(arr);
+    f(iPtr);
+    printf("%d\n", i);
 
-    for(int i = 0; i < 5; i ++){
-        printf("%d\n", arr[i]);
-    }
 }
+
+
