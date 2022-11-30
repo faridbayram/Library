3817418b1a970f0ae1f5d640cf6f4e1049698f26
faridbayram <faridbayramgit@gmail.com>
2022-12-01 01:29:41 +0400

    _

--- CProjects/Pointers/main.c
+++ CProjects/Pointers/main.c
@@ -1,42 +1,20 @@
 #include <stdio.h>
 
-void myFunc(int *arrPtr){
-    for(int i = 0; i < 3; i ++)
-    {
-        arrPtr[i] = i + 1;
+void printCharacters(const char *sPtr)
+{
+    for (; *sPtr != '\0'; ++sPtr) { // no initialization
+        printf("%c", *sPtr);
     }
-
-    int newArray[] = {5,4,3};
-    arrPtr = newArray;
-}
-
-void printArray(int arr[]){
-    for(int i = 0; i < 3; i ++)
-    {
-        printf("%d\t", arr[i]);
-    }
-    puts("");
-
-    arr[2] = 2;
-    int someOtherArr[] = {1,1,1,1,11,1};
-    arr = someOtherArr;
-}
-
-void f(int *ptr){
-    *ptr = 15;
-    int a = 1;
 }
 
 int main() {
-    int i = 3;
-    printf("%d\n", i);
-    const int *iPtr = &i;
-    int k = 3;
-    iPtr = &k;
+    int x;
+    int y;
 
-    f(iPtr);
-    printf("%d\n", i);
+    const int *const ptr;
 
+    int size = sizeof ptr;
+    printf("size of address(ptr) for this machine is : %d", size);
 }
 
 
