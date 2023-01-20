387816997c62953e4b11dfb99ff203167f11df7d
faridbayram <faridbayramgit@gmail.com>
2023-01-21 00:12:04 +0400

    _

--- CProjects/Pointers/main.c
+++ CProjects/Pointers/main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
-int main() {
-    int n;
-    printf("enter the length of array : ");
-    scanf("%d", &n);
+int main()
+{
+    int b[5] = {1,2,3,4,5};
+    int* bPtr;
 
-    int arr[n];
-    printf("%llu", sizeof(arr));
+    bPtr = b;
+
+    for(int i = 0; i < 10; i ++)
+    {
+        printf("element at %d is : %d\n", i, *(bPtr + i));
+    }
 }
