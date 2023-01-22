6d1cac848d626eb758cd3b0292259546ae14358a
faridbayram <faridbayramgit@gmail.com>
2023-01-23 01:15:16 +0400

    _

--- CProjects/Pointers/main.c
+++ CProjects/Pointers/main.c
@@ -1,31 +1,11 @@
 #include <stdio.h>
 
-#define SIZE 10
-
-void copy1(char* const s1, const char* const s2);
-void copy2(char* s1, const char* s2);
-
 int main()
 {
-    char string1[SIZE];
-    char* string2 = "Hello";
+    const char* suit[4] = {"Hearts", "Diamonds", "Clubs", "Spades"};
 
-    copy1(string1, string2);
-    printf("string1 = %s\n", string1);
 
-    char string3[SIZE];
-    char string4[] = "Good bye";
-
-    copy2(string3, string4);
-    printf("string3 = %s\n", string3);
+    for(int i = 0; i < 4; i ++){
+        printf("%c", *(suit[i] + i));
+    }
 }
-
-
-void copy1(char* const s1, const char* const s2){
-    for(size_t i = 0; (s1[i] = s2[i]) != '\0'; ++i){}
-}
-
-void copy2(char* s1, const char* s2){
-    for(; (*s1 = *s2) != '\0'; ++s1, ++s2){}
-}
-
