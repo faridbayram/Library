7652783b2e83d7523fe741fdaff92a3e02d9f6a9
faridbayram <faridbayramgit@gmail.com>
2023-01-28 02:10:42 +0400

    _

--- CProjects/Structs/main.c
+++ CProjects/Structs/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 struct MathematicalOperation {
     int operand1;
@@ -6,9 +8,22 @@ struct MathematicalOperation {
 };
 
 int main() {
-    struct MathematicalOperation op1;
-    op1.operand1 = 5;
-    op1.operand2 = 15;
+    typedef struct MathematicalOperation mo;
 
-    printf("sum of %d and %d is %d", op1.operand1, op1.operand2, op1.operand1 + op1.operand2);
+    srand(time(NULL));
+
+    mo mo1;
+    mo1.operand1 = 5;
+    mo1.operand2 = 15;
+
+    mo mo2;
+    mo2.operand1 = 32;
+    mo2.operand2 = 24;
+
+    mo moArr[2] = {mo1, mo2};
+    mo* moPtr = moArr;
+
+    for(int i = 0; i < 10; i ++, moPtr++){
+        printf("operand1 : %d, operand2 : %d\n", moPtr->operand1, moPtr->operand2);
+    }
 }
