8376a507a7d8c85ec3e27dc4fa06fc078d8312db
Farid <farid.bayramov@simbrella.com>
2023-03-06 22:00:38 +0400

    _

--- CProjects/FileProcessing/main.c
+++ CProjects/FileProcessing/main.c
@@ -1,34 +1,49 @@
 #include <stdio.h>
-
-
+#include <sys/stat.h>
+#include <time.h>
 
 int main() {
-    FILE* cfPtr;
+    char filename[] = "clients.txt";
+    struct stat filestat;
 
-    char asdf[20] = "clientsss.txt";
-
-    if((cfPtr = fopen(asdf, "w")) == NULL){
-        puts("File could not be opened");
+    if(stat(filename, &filestat) == 0) {
+        printf("File size: %lld bytes\n", filestat.st_size);
+        printf("File permissions: %o\n", filestat.st_mode & 0777);
+        printf("File creation time: %s", ctime(&filestat.st_ctime));
+        printf("File modification time: %s", ctime(&filestat.st_mtime));
     }
-    else{
-        puts("Enter the account, name and balance.");
-        puts("Enter EOF to end input.");
-        printf("%s", "? ");
-
-        unsigned int account;
-        char name[30];
-        double balance;
-
-        scanf("%d%29s%lf", &account, name, &balance);
-
-        while(!feof(stdin)){
-            fprintf(cfPtr, "%d %s %.2f\n", account, name, balance);
-            printf("%s", "? ");
-            scanf("%d%29s%lf", &account, name, &balance);
-        }
-
-        fclose(cfPtr);
+    else {
+        printf("Failed to get file metadata.\n");
     }
 
     return 0;
 }
+
+
+
+
+
+
+/*int main() {
+    FILE* cfPtr;
+    char buffer[100];
+
+    cfPtr = fopen("clients.txt", "w+");
+
+    if(cfPtr == NULL){
+        puts("File could not be opened");
+        return -1;
+    }
+
+    fseek(cfPtr, 10, SEEK_SET);
+
+    fread(buffer, 1, 10, cfPtr);
+
+    printf("Data read from file: %s\n", buffer);
+
+    fclose(cfPtr);
+
+    int i;
+    scanf("%d", &i);
+    return 0;
+}*/
